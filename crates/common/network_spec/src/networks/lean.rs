8e777a890e50cb20324883fffb0aea1496055320
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2025-11-18 01:30:48 -0500

    fix: make it so set_lean_network_spec can only be ran once in tests (#935)
    * feat: make it so set_lean_network_spec can only be ran once in tests
    
    * f

--- crates/common/network_spec/src/networks/lean.rs
+++ crates/common/network_spec/src/networks/lean.rs
@@ -1,3 +1,5 @@
+#[cfg(test)]
+use std::sync::Once;
 use std::{
     fmt::{self, Display},
     sync::{Arc, OnceLock},
@@ -8,6 +10,45 @@ use serde::Deserialize;
 
 /// Static specification of the Lean Chain network.
 pub static LEAN_NETWORK_SPEC: OnceLock<Arc<LeanNetworkSpec>> = OnceLock::new();
+
+/// MUST be called only once at the start of the application to initialize static
+/// [LeanNetworkSpec].
+///
+/// The static `LeanNetworkSpec` can be accessed using [lean_network_spec].
+///
+/// # Panics
+///
+/// Panics if this function is called more than once.
+#[cfg(not(test))]
+pub fn set_lean_network_spec(network_spec: Arc<LeanNetworkSpec>) {
+    LEAN_NETWORK_SPEC
+        .set(network_spec)
+        .expect("LeanNetworkSpec should be set only once at the start of the application");
+}
+
+#[cfg(test)]
+static HAS_NETWORK_SPEC_BEEN_INITIALIZED: Once = Once::new();
+
+#[cfg(test)]
+pub fn set_lean_network_spec(network_spec: Arc<LeanNetworkSpec>) {
+    HAS_NETWORK_SPEC_BEEN_INITIALIZED.call_once(|| {
+        LEAN_NETWORK_SPEC
+            .set(network_spec)
+            .expect("LeanNetworkSpec should be set only once at the start of the application");
+    });
+}
+
+/// Returns the static [LeanNetworkSpec] initialized by [set_lean_network_spec].
+///
+/// # Panics
+///
+/// Panics if [set_lean_network_spec] wasn't called before this function.
+pub fn lean_network_spec() -> Arc<LeanNetworkSpec> {
+    LEAN_NETWORK_SPEC
+        .get()
+        .expect("LeanNetworkSpec wasn't set")
+        .clone()
+}
 
 #[derive(Clone, Debug, Eq, PartialEq, Default)]
 pub enum Devnet {
@@ -75,30 +116,4 @@ impl LeanNetworkSpec {
             Devnet::One => matches!(target, Devnet::One),
         }
     }
-}
-
-/// MUST be called only once at the start of the application to initialize static
-/// [LeanNetworkSpec].
-///
-/// The static `LeanNetworkSpec` can be accessed using [lean_network_spec].
-///
-/// # Panics
-///
-/// Panics if this function is called more than once.
-pub fn set_lean_network_spec(network_spec: Arc<LeanNetworkSpec>) {
-    LEAN_NETWORK_SPEC
-        .set(network_spec)
-        .expect("LeanNetworkSpec should be set only once at the start of the application");
-}
-
-/// Returns the static [LeanNetworkSpec] initialized by [set_lean_network_spec].
-///
-/// # Panics
-///
-/// Panics if [set_lean_network_spec] wasn't called before this function.
-pub fn lean_network_spec() -> Arc<LeanNetworkSpec> {
-    LEAN_NETWORK_SPEC
-        .get()
-        .expect("LeanNetworkSpec wasn't set")
-        .clone()
 }
